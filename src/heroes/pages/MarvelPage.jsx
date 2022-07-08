36427086381a2b6d27a3dafa5f6bd13583648730
Jesus Reyes <luisdejesus200122@gmail.com>
2022-07-07 23:46:27 -0600

    Heroes app terminado

---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,11 @@
+import { HeroList } from "../components"
+
+export const MarvelPage = () => {
+  return (
+    <>
+    <h1>MarvelPage</h1>
+    <hr></hr>
+    <HeroList publisher="Marvel Comics"/>
+    </>
+  )
+}
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { LoginPage } from '../auth/pages/LoginPage'
+import { HeroesRoutes } from '../heroes/routes/HeroesRoutes'
+import {  Route, Routes } from 'react-router-dom'
+import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
+
+export const AppRouter = () => {
+  return (
+    <>
+   
+         <Routes>
+          <Route path='login/*' element={
+          <PublicRoute>
+            <Routes>
+              <Route path='/*' element= {<LoginPage/>}/>
+
+             
+            </Routes>
+          </PublicRoute>
+
+          }
+          >
+
+          </Route>
+        
+        <Route path="/*" element={<PrivateRoute>
+          <HeroesRoutes/>
+        </PrivateRoute>
+        } 
+        />
+
+        {/* <Route path="login" element={<LoginPage/>} /> */}
+        {/* <Route path="/*" element={<HeroesRoutes/>} /> */}
+      </Routes>
+    </>
+  )
+}
